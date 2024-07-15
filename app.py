4ec96905d8714885796951e22d2369a3f38360e7
IvaNG515 <ivan.ng0515@gmail.com>
2024-07-15 21:27:48 +0800

    big changes (more features)

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from sqlite3 import *
 from search import get_word, check_none
+from flask_session import Session
+from string import digits, ascii_letters, ascii_uppercase
 
-redirects = 0
+redirects_word = 0
+redirects_color = 0
 
 def merge(word_dict):
     pos_merge, def_merge, sentence_merge, syn_merge = "", "", "", ""
@@ -31,31 +34,55 @@ def merge(word_dict):
 
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 @app.route("/")
 def index():
-    if redirects > 0:
+    if redirects_word > 0:
         return render_template("index.html", not_found="<h5>Not a word!</h5>")
+    elif redirects_color > 0:
+        return render_template("index.html", not_found="<h5>Invalid Color!</h5>")
     else:
         return render_template("index.html", not_found="")
 
 @app.route("/word", methods=["POST"])
 def word():
-    global redirects
-    redirects = 0
+    global redirects_word
+    redirects_word = 0
 
     word = request.form.get("word").lower()
 
     if not word:
-        redirects += 1
+        redirects_word += 1
         return redirect("/")
     
     res = get_word(word)
     if res == []:
-        redirects += 1
+        redirects_word += 1
         return redirect("/")
     else:
         merge_res = merge(res)
         return render_template("word.html", word=word, pos=merge_res[0], definition=merge_res[1], sentence=merge_res[2], syn=merge_res[3])
+
+@app.route("/color", methods=["POST"])
+def colors():
+    global redirects_color
+    for i in range(len(request.form)):
+        cur_color_index = "color" + str(i + 1)
+        cur_color = request.form.get(cur_color_index)
+        if (cur_color[0] != "#") or (not cur_color) or (not len(cur_color) == 7):
+            redirects_color += 1
+            return redirect("/")
+        hex_color_check = cur_color[1:]
+        for i in range(7):
+            if (hex_color_check[i] not in digits) or (hex_color_check not in ascii_letters[:6]) or (hex_color_check not in ascii_uppercase[:6]):
+                redirects_color += 1
+                return redirect("/")
+        session[cur_color_index] = request.form.get(cur_color_index)
+    print(colors)
+    return redirect("/")
 
 @app.route("/back")
 def back():
---
+++ static/index.js
@@ -0,0 +1,17 @@
+let add_btn = document.getElementById("add_btn");
+let color_form = document.getElementById("color_form");
+
+let submit_btn = '<button type="submit" class="add_color">Change color settings</button>';
+
+let color_cnt = 3
+
+function add_color() {
+    color_cnt += 1;
+    console.log(color_cnt)
+    let color_form_inner = color_form.innerHTML;
+    let color_btn = `<input type="text" name="color${color_cnt}" class="search longer" placeholder="Color ${color_cnt} (Enter hex values)"><br>`;
+    color_form_inner = color_form_inner.replace(submit_btn, color_btn + submit_btn);
+    color_form.innerHTML = color_form_inner;
+}
+
+add_btn.addEventListener("click", add_color);
